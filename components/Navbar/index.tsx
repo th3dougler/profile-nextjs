602b41cdc4b13f99ef0de8232df1b19d2e4e5b49
Doug Jones <th3dougler@gmail.com>
2024-10-20 15:37:18 -0400

    x

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from './navbar.module.css';
+import { useRouter } from 'next/router';
 
 const Navbar: React.FC = () => {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  const homeStyle = currentRoute === '/' ? styles.active : '';
+  const utilStyle = currentRoute === '/utils' ? styles.active : '';
+  const contactStyle = currentRoute === '/contact' ? styles.active : '';
+  
   return (
     <div className={styles.navbar_container}>
       <div className={styles.header}>
@@ -12,13 +20,13 @@ const Navbar: React.FC = () => {
 
       <nav className={styles.navbar}>
         <ul>
-          <li>
+          <li className={homeStyle}>
             <Link href="/">Home</Link>
           </li>
-          <li>
+          <li className={utilStyle}>
             <Link href="/utils">Utils</Link>
           </li>
-          <li>
+          <li className={contactStyle}>
             <Link href="/contact">Contact</Link>
           </li>
         </ul>
--- pages/utils/index.tsx
+++ pages/utils/index.tsx
@@ -1,23 +1,115 @@
 import CsvConverter from '@/components/utils/CsvConverter';
 import EpochConverter from '@/components/utils/EpochConverter';
 import ListToCsv from '@/components/utils/ListToCsv';
-import styles from './index.module.css';
+import styles from './utils.module.css';
 import NanoidGenerator from '@/components/utils/NanoidGenerator';
+import { useState } from 'react';
 const UtilitiesPage = () => {
+  const [isOpen, setIsOpen] = useState<Record<string, boolean>>({
+    nanoid: false,
+    epoch: false,
+    listToCsv: false,
+    csvConverter: false,
+  });
+
+  const toggleSection = (section: string) => {
+    setIsOpen((prevState) => ({
+      ...prevState,
+      [section]: !prevState[section],
+    }));
+  };
+
+  const closeAll = () => {
+    setIsOpen((prevState) => {
+      const newState: Record<string, boolean> = {};
+      for (const key in prevState) {
+        newState[key] = false;
+      }
+      return newState;
+    });
+  };
+
   return (
     <main className={styles.main}>
       <h1>Utilities Page</h1>
+      <p>Some tools I find to be useful, perhaps you might as well</p>
+      <button onClick={closeAll}>Close All</button>
       <section className={styles.section}>
-        <NanoidGenerator />
+        <button
+          onClick={() => toggleSection('nanoid')}
+          className={styles.toggleButton}
+        >
+          <span
+            className={`${styles.caret} ${isOpen.nanoid ? styles.caretOpen : ''}`}
+          >
+            <span className={styles.caretHorizontal}></span>
+            <span className={styles.caretVertical}></span>
+          </span>
+          {isOpen.nanoid ? 'Hide' : 'Show'} Nanoid Generator
+        </button>
+        <div
+          className={`${styles.content} ${isOpen.nanoid ? styles.open : ''}`}
+        >
+          <NanoidGenerator />
+        </div>
       </section>
+
       <section className={styles.section}>
-        <EpochConverter />
+        <button
+          onClick={() => toggleSection('epoch')}
+          className={styles.toggleButton}
+        >
+          <span
+            className={`${styles.caret} ${isOpen.epoch ? styles.caretOpen : ''}`}
+          >
+            <span className={styles.caretHorizontal}></span>
+            <span className={styles.caretVertical}></span>
+          </span>
+          {isOpen.epoch ? 'Hide' : 'Show'} Epoch Converter
+        </button>
+        <div className={`${styles.content} ${isOpen.epoch ? styles.open : ''}`}>
+          <EpochConverter />
+        </div>
       </section>
+
       <section className={styles.section}>
-        <ListToCsv />
+        <button
+          onClick={() => toggleSection('listToCsv')}
+          className={styles.toggleButton}
+        >
+          <span
+            className={`${styles.caret} ${isOpen.listToCsv ? styles.caretOpen : ''}`}
+          >
+            <span className={styles.caretHorizontal}></span>
+            <span className={styles.caretVertical}></span>
+          </span>
+          {isOpen.listToCsv ? 'Hide' : 'Show'} List to CSV
+        </button>
+        <div
+          className={`${styles.content} ${isOpen.listToCsv ? styles.open : ''}`}
+        >
+          <ListToCsv />
+        </div>
       </section>
+
       <section className={styles.section}>
-        <CsvConverter />
+        <button
+          onClick={() => toggleSection('csvConverter')}
+          className={styles.toggleButton}
+        >
+          <span
+            className={`${styles.caret} ${isOpen.csvConverter ? styles.caretOpen : ''}`}
+          >
+            <span className={styles.caretHorizontal}></span>
+            <span className={styles.caretVertical}></span>
+          </span>
+          {isOpen.csvConverter ? 'Hide' : 'Show'} CSV Converter
+        </button>
+        <div
+          className={`${styles.content} ${isOpen.csvConverter ? styles.open : ''}`}
+        >
+          <CsvConverter />
+        </div>
       </section>
     </main>
   );
